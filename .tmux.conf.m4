saj/dotfiles

commit 0feda60c05c41402b93e2548afc8efad1eebf1f7

Have new panes and windows open in my CWD
Requires tmux 1.9.

.tmux.conf.m4
@@ -42,8 +42,8 @@ unbind %
 unbind '"'
 
 # Use these instead.
-bind - split-window -v
-bind \ split-window -h
+bind - split-window -v -c '#{pane_current_path}'
+bind \ split-window -h -c '#{pane_current_path}'
 
 # It can be VERY frustrating to hit this key by accident.  (By default, 
 # space will rearrange panes.)  Especially after spending an inordinate 
@@ -51,10 +51,15 @@ bind \ split-window -h
 unbind Space
 
 bind C-a last-window
+unbind c
+bind c new-window -c '#{pane_current_path}'
+
 ifelse(os_name, `Darwin', `dnl
 bind C-c run "tmux save-buffer - | reattach-to-user-namespace pbcopy"
 bind C-v run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"dnl
 ')
-bind m command-prompt "split-window 'exec man %%'"
+
 bind r source-file ~/.tmux.conf
+
+bind m command-prompt "split-window 'exec man %%'"
 bind S command-prompt "new-window -n %1 'ssh %1'"

commit 6faa8f7fa9f41d7e02f23b5ac5c9e381ae84921d

tmux: Only supply -c against newer tmux releases
No matter which version of tmux is installed, new panes should be
created with a working directory set to the working directory of the
process running in the active pane.

tmux releases prior to 1.9 did this out of the box.

tmux releases since 1.9 require the -c flag (see commit 0feda60c).

The -c flag was not available in previous tmux releases.  0feda60c broke
.tmux.conf for, e.g., tmux 1.6, which I still use in places.

.tmux.conf.m4
@@ -1,12 +1,33 @@
+# This configuration must remain backwards compatible with tmux 1.6.
+# (I still have EL6 machines with tmux installed from EPEL.)
+
 divert(-1)
 
-define(`os_name', patsubst(esyscmd(`uname -s'), `
-'))
+dnl  This divert(-1)...divert`'dnl m4 pattern is used to suppress spurious, but
+dnl  syntactically harmless, blank lines in our output.
 
-divert`'dnl
-ifelse(os_name, `Darwin', `dnl
-set  -g  default-command "reattach-to-user-namespace -l zsh"dnl
-')
+changequote(`[', `]')
+
+define([os_name], patsubst(esyscmd([uname -s]), [
+]))
+
+define([tmux_major_version], patsubst(esyscmd([tmux -V | sed 's/^tmux //' | awk -F . '{print $1}']), [
+]))
+
+define([tmux_minor_version], patsubst(esyscmd([tmux -V | sed 's/^tmux //' | awk -F . '{print $2}']), [
+]))
+
+dnl  Feature flag: default-path (removed in tmux 1.9)
+ifelse(tmux_major_version, [1], [
+ifelse(patsubst(esyscmd([test "]tmux_minor_version[" -lt 9; echo $?]), [
+]), [0], [
+define([tmux_has_feature_default_path], [1])
+]]))
+
+divert[]dnl
+ifelse(os_name, [Darwin], [dnl
+set  -g  default-command "reattach-to-user-namespace -l zsh"
+])dnl
 set  -g  default-terminal "screen-256color"
 
 set  -gs escape-time 0
@@ -40,8 +61,16 @@ unbind %
 unbind '"'
 
 # Use these instead.
+ifdef([tmux_has_feature_default_path], [dnl
+bind - split-window -v
+], [dnl
 bind - split-window -v -c '#{pane_current_path}'
+])dnl
+ifdef([tmux_has_feature_default_path], [dnl
+bind \ split-window -h
+], [dnl
 bind \ split-window -h -c '#{pane_current_path}'
+])dnl
 
 # It can be VERY frustrating to hit this key by accident.  (By default, 
 # space will rearrange panes.)  Especially after spending an inordinate 
@@ -50,13 +79,17 @@ unbind Space
 
 bind C-a last-window
 unbind c
+ifdef([tmux_has_feature_default_path], [dnl
+bind c new-window
+], [dnl
 bind c new-window -c '#{pane_current_path}'
+])dnl
 
-ifelse(os_name, `Darwin', `dnl
+ifelse(os_name, [Darwin], [dnl
 bind C-c run "tmux save-buffer - | reattach-to-user-namespace pbcopy"
-bind C-v run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"dnl
-')
+bind C-v run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"
 
+])dnl
 bind r source-file ~/.tmux.conf
 
 bind m command-prompt "split-window 'exec man %%'"